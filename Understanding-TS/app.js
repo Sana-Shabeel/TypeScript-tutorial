3e162c3eba8dbb56ca24b8ffb37b48dcc3e82f47
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-09-15 22:46:43 +0200

    Working with core data types

--- Understanding-TS/app.js
+++ Understanding-TS/app.js
@@ -1,1 +1,5 @@
-console.log("Here we gooo...");
+// The main goal is to get the errors during development so we can easily them
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(5, 2));
--- Understanding-TS/app.ts
+++ Understanding-TS/app.ts
@@ -1,1 +1,7 @@
-console.log("Here we gooo...");
+// The main goal is to get the errors during development so we can easily them
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+console.log(add(5, 2));
