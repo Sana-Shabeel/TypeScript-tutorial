f7f404b0e9978b4b93043ea3a28cfb02fe53cac2
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-09-26 07:35:42 +0200

    recreating padLeft function

--- Understanding-TS/app.js
+++ Understanding-TS/app.js
@@ -1,31 +1,14 @@
-// Enums or enumerations are a new data type supported in TypeScript. Most object-oriented languages like Java and C# use enums. This is now available in TypeScript too.
-// In simple words, enums allow us to declare a set of named constants i.e. a collection of related values that can be numeric or string values.
-// There are three types of enums:
-// Numeric enum
-// String enum
-// Heterogeneous enum
-var Position;
-(function (Position) {
-    Position[Position["ADMIN"] = 0] = "ADMIN";
-    Position[Position["READ_ONLY"] = 100] = "READ_ONLY";
-    Position["AUTHOR"] = "AUTHOR";
-})(Position || (Position = {}));
-var person = {
-    name: "John",
-    age: 36,
-    hobbies: ["Sports", "Cooking"],
-    role: [2, "developer"],
-    position: Position.AUTHOR
-};
-person.role.push("another one"); // this will work and add an extra role. / exception !!
-// person.role = ["another" 2] this will not work
-console.log(person.position);
-// let the var numbers be a number array
-var numbers;
-numbers = [2];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toLocaleUpperCase()); // cause typescript knows that hobby is a string
+/*
+UNION TYPES
+
+
+Occasionally, you’ll run into a library that expects a parameter to be either a number or a string. For instance, take the following function:
+* Takes a string and adds "padding" to the left.
+* If 'padding' is a string, then 'padding' is appended to the left side.
+* If 'padding' is a number, then that number of spaces is added to the left side.
+ 
+*/
+function padLeft(value, padding) {
+    return value + "-".repeat(padding);
 }
-// TUPLE
+console.log(padLeft("Hello world", 4));
--- Understanding-TS/app.ts
+++ Understanding-TS/app.ts
@@ -1,47 +1,16 @@
-// Enums or enumerations are a new data type supported in TypeScript. Most object-oriented languages like Java and C# use enums. This is now available in TypeScript too.
+/*
+UNION TYPES 
 
-// In simple words, enums allow us to declare a set of named constants i.e. a collection of related values that can be numeric or string values.
 
-// There are three types of enums:
+Occasionally, you’ll run into a library that expects a parameter to be either a number or a string. For instance, take the following function:
+* Takes a string and adds "padding" to the left.
+* If 'padding' is a string, then 'padding' is appended to the left side.
+* If 'padding' is a number, then that number of spaces is added to the left side.
+ 
+*/
 
-// Numeric enum
-// String enum
-// Heterogeneous enum
-
-enum Position {
-  ADMIN,
-  READ_ONLY = 100,
-  AUTHOR = "AUTHOR",
+function padLeft(value: string, padding: any): string {
+  return value + "-".repeat(padding);
 }
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[]; // array of strings
-  role: [number, string]; // a tuple == will enforce the length and the types
-  position: Position; // enum
-} = {
-  name: "John",
-  age: 36,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "developer"],
-  position: Position.AUTHOR,
-};
-
-person.role.push("another one"); // this will work and add an extra role. / exception !!
-// person.role = ["another" 2] this will not work
-
-console.log(person.position);
-
-// let the var numbers be a number array
-let numbers: number[];
-
-numbers = [2];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toLocaleUpperCase()); // cause typescript knows that hobby is a string
-}
-
-// TUPLE
+console.log(padLeft("Hello world", 4));
